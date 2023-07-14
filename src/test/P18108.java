ae8bbe6c586515c226a57fc476b93b3d8004c5bd
deliciousdev <deliciousdev@naver.com>
2023-07-14 14:10:47 +0900

    7/14 BOJ - 18108

---
+++ src/test/P18108.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.util.*;
+import java.io.*;
+
+public class P18108 {
+
+    static FastReader sc= new FastReader();
+    static StringBuilder sb= new StringBuilder();
+
+
+    public static void main(String[] args){
+        final int term = 2541-1998;
+
+        System.out.print(sc.nextInt()-term);
+    }
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        FastReader(){
+            br= new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null||!st.hasMoreElements()){
+                try{
+                    st= new StringTokenizer(br.readLine());
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        String nextLine(){
+            String str="";
+            try{
+                str= br.readLine();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+}
