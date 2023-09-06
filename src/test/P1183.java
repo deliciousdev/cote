bd89e9d236a904f4ac8191fb561c8124266a3109
deliciousdev <deliciousdev@naver.com>
2023-09-06 14:10:02 +0900

    9/06 BOJ - 1183

--- src/test/P1183.java
+++ src/test/P1183.java
@@ -15,7 +15,42 @@ public class P1183 {
     public static void main(String[] args){
         n=sc.nextInt();
 //        solve1();//중근 위치 중근 고려 하면 잘못된 풀이임 : 절대값 그래프 개형으로 접근
-        solve2();//논리적으로 기준점을 왼쪽 or 오른쪽으로 이동했을때 어떤 지점으로 이동해야 최소값이 되는가를 논리적으로 생각해봄.
+//        solve2();//논리적으로 기준점을 왼쪽 or 오른쪽으로 이동했을때 어떤 지점으로 이동해야 최소값이 되는가를 논리적으로 생각해봄.
+        solve3();//왼쪽인덱스부터 정답을 찾을때까지 오르쪽으로 이동하는것을 시뮬레이션
+    }
+
+    static void solve3(){
+        int[] points = new int[n];
+        for(int i=0; i<n; ++i){
+            int a= sc.nextInt();
+            int b= sc.nextInt();
+            points[i]=b-a;
+        }
+
+        Arrays.sort(points);
+
+        int idx=-1;
+        int left=0;
+        int right=n-left;
+
+        int begin=-1;
+        while(left<=right){
+            ++idx;
+            while( idx+1<n && points[idx]==points[idx+1]){
+                ++idx;
+            }
+            left=idx+1;
+            right=n-left;
+
+            if(left<right){
+                begin=idx;
+            }
+        }
+        int end=idx;
+        int beginValue=points[begin+1];
+        int endValue=points[end];
+        System.out.print(endValue-beginValue+1);
+
     }
 
     static void solve2(){
